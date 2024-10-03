086036728c43239d6494de66344d1e88b36e92b3
eotkd4791 <eotkd4791@gmail.com>
2024-10-03 21:30:25 +0900

    블로그 설정 변경

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,9 @@ export const SITE: Site = {
   author: "유대상",
   desc: "유대상의 블로그",
   title: "개발F념",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "og-image.png",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 20,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
@@ -18,7 +18,7 @@ export const LOCALE = {
 
 export const LOGO_IMAGE = {
   enable: false,
-  svg: true,
+  svg: false,
   width: 216,
   height: 46,
 };
@@ -27,19 +27,19 @@ export const SOCIALS: SocialObjects = [
   {
     name: "Github",
     href: "https://github.com/eotkd4791",
-    linkTitle: ` ${SITE.title} on Github`,
+    linkTitle: ` ${SITE.author} on Github`,
     active: true,
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/daesang-stefan-you",
-    linkTitle: `${SITE.title} on LinkedIn`,
+    linkTitle: `${SITE.author} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
     href: "mailto:eotkd4791@kakao.com",
-    linkTitle: `Send an email to ${SITE.title}`,
+    linkTitle: `Send an email to ${SITE.author}`,
     active: true,
   },
 ];
